a06293e479800de816dda521c58623d25b596800
Gabriel Mayan <gabrielmayan@gmail.com>
2021-08-30 20:13:08 -0300

    Ajustes para upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const express = require('express');
 const app = express();
 const rotas = require('./routes');
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 app.use(rotas);
 
